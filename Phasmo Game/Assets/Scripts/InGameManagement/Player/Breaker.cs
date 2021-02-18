87332e26db6e519fa7e7e2f870f999a1e43e9d31
Beaublom4 <Beaublom4@gmail.com>
2021-02-18 02:40:18 +0100

    Herres in je scene

---
+++ Phasmo Game/Assets/Scripts/InGameManagement/Player/Breaker.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Breaker : MonoBehaviour
+{
+    public string[] Puzzles;
+
+    public GameObject countUp;
+    public List<GameObject> CountUp_ButtonRandomized = new List<GameObject>();
+    public int buttonNum;
+    public void Use()
+    {
+        int randomNum = Random.Range(0, Puzzles.Length);
+        Invoke(Puzzles[randomNum], 0);
+    }
+    void CountUp()
+    {
+        countUp.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        foreach(Transform child in countUp.transform)
+        {
+            AddToList();
+        }
+        for (int i = 0; i < CountUp_ButtonRandomized.Count; i++)
+        {
+            CountUp_ButtonRandomized[i].GetComponentInChildren<TMP_Text>().text = (i + 1).ToString();
+        }
+    }
+    void AddToList()
+    {
+        int randomNum = Random.Range(0, countUp.transform.childCount);
+        GameObject g = countUp.transform.GetChild(randomNum).gameObject;
+        if (!CountUp_ButtonRandomized.Contains(g))
+        {
+            CountUp_ButtonRandomized.Add(g);
+        }
+        else
+        {
+            AddToList();
+        }
+    }
+    public void ClickCountUp(TMP_Text text)
+    {
+        if (int.Parse(text.text) - 1 == buttonNum)
+        {
+            print("Good");
+            buttonNum++;
+            if(buttonNum == CountUp_ButtonRandomized.Count)
+            {
+                print("Finished");
+            }
+        }
+        else
+        {
+            print("Wrong");
+        }
+    }
+}
---
+++ Phasmo Game/Assets/Scripts/MainMenu/Account.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class Account : MonoBehaviour
+{
+    public TMP_Text playerName, playerLv, playerLvXp;
+    public Slider lvSlider;
+
+    public Acount acountScript;
+
+    private void OnEnable()
+    {
+        playerName.text = PhotonNetwork.LocalPlayer.NickName;
+        playerLv.text = "LV:" + acountScript.playerLevel.ToString();
+        playerLvXp.text = acountScript.xp.ToString() + "/" + acountScript.wantedXP.ToString();
+        lvSlider.maxValue = acountScript.wantedXP;
+        lvSlider.value = acountScript.xp;
+    }
+}
